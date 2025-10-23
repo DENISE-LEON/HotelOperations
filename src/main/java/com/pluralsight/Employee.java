15d7f9d9844196a35041bca9c99840528878ec53
deniseleon <kbenisee7@gmail.com>
2025-10-22 22:55:08 -0400

    created arraylist for shifts(stores duration of each shift) updated hours worked getter to stream through the arraylist of shifts and get the total updated punch out to add the duration to arraylist and reset the punches to be ready for the next shift

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -2,8 +2,13 @@ package com.pluralsight;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Employee {
+    //creating an arraylist to store the employee shift durations
+    //if punchcard class created: pass in the class name
+    private ArrayList<Duration> shifts = new ArrayList<>();
+
     private int employeeId;
     private String name;
     private String department;
@@ -28,8 +33,11 @@ public class Employee {
     }
     //turning get hours worked into a derived getter that returns hours based on start and end time
     public double getHoursWorked() {
-        Duration hoursWorked = Duration.between(startTime, endTime);
-        return hoursWorked.toMinutes() /60.0;
+       return shifts.stream()
+               //double output wanted for accuracy (with minutes)
+               .mapToDouble(d-> d.toMinutes() /60)
+               .sum();
+
     }
     //derived getters
     public double getTotalPay() {
@@ -81,9 +89,20 @@ public class Employee {
         if (this.startTime == null) {
             System.out.println("You have not punched in");
         } else {
+            //sets the end date to time of punch out
             this.endTime = LocalDateTime.now();
+            //adds the duration of the shift to the arraylist
+            shifts.add (Duration.between(startTime, endTime));
+            //must set the times to null to prevent overwriting everytime punch out
+            startTime = null;
+            endTime = null;
             System.out.println("You have punched out. Go be happy now");
         }
     }
 
 }
+//ideas - extra
+//create shifts array list
+//use streams to add the shifts
+//move punchcard stuff into a punchcard method
+//create a method that resets the hours worked for the pay period (2x month or monthly)
